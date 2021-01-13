3a9e5c5bf226441050e63c931186a3ea526500e3
ethanvachon <ethanvachon@gmail.com>
2021-01-12 18:43:46 -0700

    getting there

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,8 @@
-import ValueSchema from "../models/Value";
 import mongoose from "mongoose";
+import CarSchema from "../models/Car"
 
 class DbContext {
-  Values = mongoose.model("Value", ValueSchema);
+  Cars = mongoose.model("Car", CarSchema)
 }
 
 export const dbContext = new DbContext();
